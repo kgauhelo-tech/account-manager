fba5af817930d947f0140d817bad919bba85d44c
Kgauhelo Codes <lebakeng.sirr@gmail.com>
2026-09-15 21:29:39 +0200

    Completed the code and the output should be clear as soon as the code is running

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-import readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
-import { log, time } from "node:console";
-
-// const rl = readline.createInterface({ input, output });
-
-// // Ask the user a question
-// const name = await rl.question("What is your name? ");
-// console.log(`Hello, ${name}!`);
-
-// // Always close the interface when done
-// rl.close();
-
 //Creating the class
 class BankAccount {
   constructor(username) {
     this.username = username;
     this._accountBalance = 0.0;
     this.accountNumber = generateAccountNumber();
-    this.lastWithdrawalRequestTime;
+    this.lastWithdrawalRequestTime = 0;
+    this.averageTransaction = [];
   }
 
   //done
@@ -44,23 +32,39 @@ class BankAccount {
     }
   }
 
-  //done
   transfer(transferAmount, targetUser) {
-    if (transferAmount > 0 && transferAmount <= this.accountBalance) {
-      transferFunds(targetUser, transferAmount);
-      transferAmount = this.accountBalance = -transferAmount;
-      return console.log(
-        `\nTransfer successful. \nNew Account Balance: ${this.accountBalance}`,
-      );
+    let averageTransaction =
+      this.averageTransaction.reduce((acc, value) => acc + value, 0) / 2;
+    let isAboveAverageTransaction = transferAmount > averageTransaction * 5;
+
+    // We take 5 transactions to create the base line
+    if (isAboveAverageTransaction && this.averageTransaction.length > 5) {
+      return console.log(`\nTransfer unsuccessful.\nThis might be a scam`);
     } else {
-      return console.log(`\nTransfer unsuccessful.`);
+      if (transferAmount > 0 && transferAmount <= this.accountBalance) {
+        transferFunds(targetUser, transferAmount);
+        transferAmount = this.accountBalance = -transferAmount;
+        this.averageTransaction.push(transferAmount);
+        return console.log(
+          `\nTransfer successful. \nNew Account Balance: ${this.accountBalance}`,
+        );
+      } else {
+        return console.log(`\nTransfer unsuccessful.`);
+      }
     }
   }
-
+  // check the time here before
   withdraw(requestAmount) {
-    if (requestAmount <= 0 && requestAmount !== 0) {
+    const now = new Date();
+    //The last condition works but I don't really get why it works.
+    // I struggled with it a lot
+    if (
+      requestAmount !== 0 &&
+      requestAmount <= this.accountBalance &&
+      now > this.lastWithdrawalRequestTime + 10
+    ) {
       this.accountBalance = -requestAmount;
-      lastWithdrawalRequestTime = time;
+      this.lastWithdrawalRequestTime = now;
       return console.log(
         `\nWithdrawal successful.\nNew Balance is: ${this.accountBalance}`,
       );
@@ -81,6 +85,7 @@ function transferFunds(user, amount) {
   bankUsers.filter((account) => account.username === user);
   return user.deposit(amount);
 }
+
 //Storage for bank accounts
 const bankUsers = [];
 
@@ -107,12 +112,16 @@ console.log(`The account balance is: ${userBob.accountBalance}`);
 userBob.deposit(1000);
 
 // Alice's details
-// console.log();
-// console.log(`The account holder is: ${userAlice.username}`);
-// console.log(`The account number is: ${userAlice.accountNumber}`);
-// console.log(`The account balance is: ${userAlice.accountBalance}`);
+console.log();
+console.log(`The account holder is: ${userAlice.username}`);
+console.log(`The account number is: ${userAlice.accountNumber}`);
+console.log(`The account balance is: ${userAlice.accountBalance}`);
 
-// userBob.transfer(500, userAlice);
+//testing withdrawal and testing the time condition
+userBob.withdraw(100);
+userBob.withdraw(100);
 
-// console.log(userAlice.accountBalance);
-// console.log(userBob.accountBalance);
+userBob.transfer(500, userAlice);
+
+console.log(`\nAccount balance for Alice is:  ${userAlice.accountBalance}`);
+console.log(`\nAccount balance for Bob is: ${userBob.accountBalance}`);
